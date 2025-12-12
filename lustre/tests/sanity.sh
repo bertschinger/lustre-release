bertschinger/lustre-release

commit a15eb4f13224e148810015896101b2950c85adff

LU-13055 mdd: per-user changelog names and mask
Allow specifying a name for newly-registered changelog users,
rather than the default "clNNN" that is otherwise used. This
allows services to register a "well-known" changelog user,
rather than having to store the changelog username in HA storage
outside of the filesystem.

Each changelog user still has a unique ID appended to it, to allow
the changelog_clear and changelog_deregister commands to be run
using only the ID if necessary/desired. User name can be used to
deregister. User name is also unique per server.

If no name is given, then default "cl" format is used.

With this new functionality, it is possible to specify the name like:
 # lctl --device testfs-MDT0000 changelog_register --user watcher
   testfs-MDT0000: Registered changelog userid 'cl13-watcher'

Per-user mask is also added to allow specific operation logging on
per-user basis. Mask can be set only during registration. Resulting
mask from per-server mask and all user masks is used for current
changelog operations.

Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
Change-Id: I56028f54cc97bbc9af03fd6559c19ef854f759d8
Reviewed-on: https://review.whamcloud.com/43380
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: John L. Hammond <jhammond@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/tests/sanity.sh
@@ -15296,8 +15296,6 @@ test_160a() {
 	ln -s $DIR/$tdir/pics/2008/portland.jpg $DIR/$tdir/pics/desktop.jpg
 	rm $DIR/$tdir/pics/desktop.jpg
 
-	changelog_dump | tail -10
-
 	echo "verifying changelog mask"
 	changelog_chmask "-MKDIR"
 	changelog_chmask "-CLOSE"
@@ -15311,7 +15309,6 @@ test_160a() {
 	test_mkdir -p $DIR/$tdir/pics/2008/sofia		# mkdir 1
 	echo "zzzzzz" > $DIR/$tdir/pics/zach/file		# open 3
 
-	changelog_dump | tail -10
 	MKDIRS=$(changelog_dump | grep -c "MKDIR")
 	CLOSES=$(changelog_dump | grep -c "CLOSE")
 	[ $MKDIRS -eq 1 ] || error "MKDIR changelog mask count $MKDIRS != 1"
@@ -15374,10 +15371,10 @@ test_160a() {
 		error "User '$cl_user' still in changelog_users"
 
 	# lctl get_param -n mdd.*.changelog_users
-	# current index: 144
+	# current_index: 144
 	# ID    index (idle seconds)
-	# cl3   144 (2)
-	if ! changelog_users $SINGLEMDS | grep "^cl"; then
+	# cl3   144   (2) mask=<list>
+	if [ -z "$(changelog_users $SINGLEMDS | grep -v current.index)" ]; then
 		# this is the normal case where all users were deregistered
 		# make sure no new records are added when no users are present
 		local last_rec1=$(changelog_users $SINGLEMDS |
@@ -16195,6 +16192,72 @@ test_160n() {
 	[[ $? -eq 0 ]] || error "fail to cancel record for $cl_user2"
 }
 run_test 160n "Changelog destroy race"
+
+test_160o() {
+	local mdt="$(facet_svc $SINGLEMDS)"
+
+	[[ $PARALLEL != "yes" ]] || skip "skip parallel run"
+	remote_mds_nodsh && skip "remote MDS with nodsh"
+	[ $MDS1_VERSION -ge $(version_code 2.14.52) ] ||
+		skip "Need MDS version at least 2.14.52"
+
+	changelog_register --user test_160o -m unlnk+close+open ||
+		error "changelog_register failed"
+	# drop server mask so it doesn't interfere
+	do_facet $SINGLEMDS $LCTL --device $mdt \
+				changelog_register -u "Tt3_-#" &&
+		error "bad symbols in name should fail"
+
+	do_facet $SINGLEMDS $LCTL --device $mdt \
+				changelog_register -u test_160o &&
+		error "the same name registration should fail"
+
+	do_facet $SINGLEMDS $LCTL --device $mdt \
+			changelog_register -u test_160toolongname &&
+		error "too long name registration should fail"
+
+	changelog_chmask "MARK+HSM"
+	lctl get_param mdd.*.changelog*mask
+	local cl_user="${CL_USERS[$SINGLEMDS]%% *}"
+	changelog_users $SINGLEMDS | grep -q $cl_user ||
+		error "User $cl_user not found in changelog_users"
+	#verify username
+	echo $cl_user | grep -q test_160o ||
+		error "User $cl_user has no specific name 'test160o'"
+
+	# change something
+	changelog_clear 0 || error "changelog_clear failed"
+	# generate some changelog records to accumulate on MDT0
+	test_mkdir -p -i0 -c1 $DIR/$tdir || error "test_mkdir $tdir failed"
+	touch $DIR/$tdir/$tfile			# open 1
+
+	OPENS=$(changelog_dump | grep -c "OPEN")
+	[[ $OPENS -eq 1 ]] || error "OPEN changelog mask count $OPENS != 1"
+
+	# must be no MKDIR it wasn't set as user mask
+	MKDIR=$(changelog_dump | grep -c "MKDIR")
+	[[ $MKDIR -eq 0 ]] || error "MKDIR changelog mask found $MKDIR > 0"
+
+	oldmask=$(do_facet $SINGLEMDS $LCTL get_param \
+				mdd.$mdt.changelog_current_mask -n)
+	# register maskless user
+	changelog_register || error "changelog_register failed"
+	# effective mask should be not changed because it is not minimal
+	mask=$(do_facet $SINGLEMDS $LCTL get_param \
+				mdd.$mdt.changelog_current_mask -n)
+	[[ $mask == $oldmask ]] || error "mask was changed: $mask vs $oldmask"
+	# set server mask to minimal value
+	changelog_chmask "MARK"
+	# check effective mask again, should be treated as DEFMASK now
+	mask=$(do_facet $SINGLEMDS $LCTL get_param \
+				mdd.$mdt.changelog_current_mask -n)
+	[[ $mask == *"HLINK"* ]] || error "mask is not DEFMASK as expected"
+
+	do_facet $SINGLEMDS $LCTL --device $mdt \
+				changelog_deregister -u test_160o ||
+		error "cannot deregister by name"
+}
+run_test 160o "changelog user name and mask"
 
 test_160p() {
 	remote_mds_nodsh && skip "remote MDS with nodsh" && return

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -2733,6 +2733,7 @@ enum llog_op_type {
 	/* LLOG_JOIN_REC	= LLOG_OP_MAGIC | 0x50000, obsolete  1.8.0 */
 	CHANGELOG_REC		= LLOG_OP_MAGIC | 0x60000,
 	CHANGELOG_USER_REC	= LLOG_OP_MAGIC | 0x70000,
+	CHANGELOG_USER_REC2	= LLOG_OP_MAGIC | 0x70002,
 	HSM_AGENT_REC		= LLOG_OP_MAGIC | 0x80000,
 	UPDATE_REC		= LLOG_OP_MAGIC | 0xa0000,
 	LLOG_HDR_MAGIC		= LLOG_OP_MAGIC | 0x45539,
@@ -2833,9 +2834,9 @@ struct llog_size_change_rec {
 #define CHANGELOG_MAGIC 0xca103000
 
 /** \a changelog_rec_type's that can't be masked */
-#define CHANGELOG_MINMASK (1 << CL_MARK)
+#define CHANGELOG_MINMASK BIT(CL_MARK)
 /** bits covering all \a changelog_rec_type's */
-#define CHANGELOG_ALLMASK 0XFFFFFFFF
+#define CHANGELOG_ALLMASK (BIT(CL_LAST) - 1)
 /** default \a changelog_rec_type mask. Allow all of them, except
  * CL_ATIME since it can really be time consuming, and not necessary
  * under normal use.
@@ -2843,8 +2844,7 @@ struct llog_size_change_rec {
  * be costly and only necessary for audit purpose.
  */
 #define CHANGELOG_DEFMASK (CHANGELOG_ALLMASK & \
-			   ~(1 << CL_ATIME | 1 << CL_OPEN | 1 << CL_GETXATTR | \
-			     1 << CL_DN_OPEN))
+	~(BIT(CL_ATIME) | BIT(CL_OPEN) | BIT(CL_GETXATTR) | BIT(CL_DN_OPEN)))
 
 /* changelog llog name, needed by client replicators */
 #define CHANGELOG_CATALOG "changelog_catalog"
@@ -2862,6 +2862,8 @@ struct llog_changelog_rec {
 } __attribute__((packed));
 
 #define CHANGELOG_USER_PREFIX "cl"
+#define CHANGELOG_USER_NAMELEN 16 /* base name including NUL terminator */
+#define CHANGELOG_USER_NAMELEN_FULL 30 /* basename plus 'cl$ID-' prefix */
 
 struct llog_changelog_user_rec {
 	struct llog_rec_hdr   cur_hdr;
@@ -2871,6 +2873,21 @@ struct llog_changelog_user_rec {
 	__u32                 cur_time;
 	__u64                 cur_endrec;
 	struct llog_rec_tail  cur_tail;
+} __attribute__((packed));
+
+/* this is twice the size of CHANGELOG_USER_REC */
+struct llog_changelog_user_rec2 {
+	struct llog_rec_hdr	cur_hdr;
+	__u32			cur_id;
+	/* only for use in relative time comparisons to detect idle users */
+	__u32			cur_time;
+	__u64			cur_endrec;
+	__u32			cur_mask;
+	__u32			cur_padding1;
+	char			cur_name[CHANGELOG_USER_NAMELEN];
+	__u64			cur_padding2;
+	__u64			cur_padding3;
+	struct llog_rec_tail	cur_tail;
 } __attribute__((packed));
 
 enum agent_req_status {

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -1567,7 +1567,7 @@ enum changelog_rec_type {
 	CL_RESYNC   = 22, /* FLR: file was resync-ed */
 	CL_GETXATTR = 23,
 	CL_DN_OPEN  = 24, /* denied open */
-	CL_LAST
+	CL_LAST,
 };
 
 static inline const char *changelog_type2str(int type) {

lustre/mdd/mdd_internal.h
@@ -95,7 +95,8 @@
 struct mdd_changelog {
 	spinlock_t		mc_lock;	/* for index */
 	int			mc_flags;
-	int			mc_mask;
+	__u32			mc_proc_mask; /* per-server mask set via parameters */
+	__u32			mc_current_mask; /* combined global+users */
 	__u64			mc_index;
 	ktime_t			mc_starttime;
 	spinlock_t		mc_user_lock;
@@ -434,6 +435,10 @@ int mdd_generic_thread_start(struct mdd_generic_thread *thread,
 void mdd_generic_thread_stop(struct mdd_generic_thread *thread);
 int mdd_changelog_user_purge(const struct lu_env *env, struct mdd_device *mdd,
 			     __u32 id);
+char *mdd_chlg_username(struct llog_changelog_user_rec2 *rec, char *buf,
+			size_t len);
+__u32 mdd_chlg_usermask(struct llog_changelog_user_rec2 *rec);
+int mdd_changelog_recalc_mask(const struct lu_env *env, struct mdd_device *mdd);
 
 /* mdd_prepare.c */
 int mdd_compat_fixes(const struct lu_env *env, struct mdd_device *mdd);
@@ -840,7 +845,7 @@ static inline bool mdd_changelog_enabled(const struct lu_env *env,
 	const struct lu_ucred *uc;
 
 	if ((mdd->mdd_cl.mc_flags & CLM_ON) &&
-	    (mdd->mdd_cl.mc_mask & BIT(type))) {
+	    (mdd->mdd_cl.mc_current_mask & BIT(type))) {
 		uc = lu_ucred_check(env);
 
 		return uc != NULL ? uc->uc_enable_audit : true;

lustre/mdd/mdd_lproc.c
@@ -83,13 +83,28 @@ static ssize_t atime_diff_store(struct kobject *kobj,
 LUSTRE_RW_ATTR(atime_diff);
 
 /**** changelogs ****/
+static int mdd_changelog_current_mask_seq_show(struct seq_file *m, void *data)
+{
+	struct mdd_device *mdd = m->private;
+	int i = 0;
+
+	while (i < CL_LAST) {
+		if (mdd->mdd_cl.mc_current_mask & BIT(i))
+			seq_printf(m, "%s ", changelog_type2str(i));
+		i++;
+	}
+	seq_putc(m, '\n');
+	return 0;
+}
+LDEBUGFS_SEQ_FOPS_RO(mdd_changelog_current_mask);
+
 static int mdd_changelog_mask_seq_show(struct seq_file *m, void *data)
 {
 	struct mdd_device *mdd = m->private;
 	int i = 0;
 
 	while (i < CL_LAST) {
-		if (mdd->mdd_cl.mc_mask & BIT(i))
+		if (mdd->mdd_cl.mc_proc_mask & BIT(i))
 			seq_printf(m, "%s ", changelog_type2str(i));
 		i++;
 	}
@@ -105,6 +120,9 @@ mdd_changelog_mask_seq_write(struct file *file, const char __user *buffer,
 	struct mdd_device *mdd = m->private;
 	char *kernbuf;
 	int rc;
+	int oldmask = mdd->mdd_cl.mc_proc_mask;
+	int newmask = oldmask;
+
 	ENTRY;
 
 	if (count >= PAGE_SIZE)
@@ -116,10 +134,48 @@ mdd_changelog_mask_seq_write(struct file *file, const char __user *buffer,
 		GOTO(out, rc = -EFAULT);
 	kernbuf[count] = 0;
 
-	rc = cfs_str2mask(kernbuf, changelog_type2str, &mdd->mdd_cl.mc_mask,
+	/* if the new mask is relative and proc mask is minimal then assume
+	 * it is relative to DEFMASK, otherwise apply new mask on the current
+	 * proc mask.
+	 */
+	if (oldmask == CHANGELOG_MINMASK) {
+		char *str = kernbuf;
+
+		while (isspace(*str))
+			str++;
+		if (*str == '+' || *str == '-')
+			newmask = CHANGELOG_DEFMASK;
+	}
+
+	rc = cfs_str2mask(kernbuf, changelog_type2str, &newmask,
 			  CHANGELOG_MINMASK, CHANGELOG_ALLMASK);
-	if (rc == 0)
+	if (rc)
+		GOTO(out, rc);
+
+	mdd->mdd_cl.mc_proc_mask = newmask;
+
+	/* if mask keeps all bits from oldmask then just extend the current
+	 * mask, otherwise the current mask should be recalculated through
+	 * all user masks.
+	 */
+	if ((newmask & oldmask) == oldmask) {
+		spin_lock(&mdd->mdd_cl.mc_user_lock);
+		mdd->mdd_cl.mc_current_mask |= newmask;
+		spin_unlock(&mdd->mdd_cl.mc_user_lock);
+	} else {
+		struct lu_env env;
+
+		rc = lu_env_init(&env, LCT_LOCAL);
+		if (rc)
+			GOTO(out, rc);
+
+		mdd_changelog_recalc_mask(&env, mdd);
+		lu_env_fini(&env);
+	}
+
+	if (!rc)
 		rc = count;
+
 out:
 	OBD_FREE(kernbuf, PAGE_SIZE);
 	return rc;
@@ -130,16 +186,34 @@ static int lprocfs_changelog_users_cb(const struct lu_env *env,
 				      struct llog_handle *llh,
 				      struct llog_rec_hdr *hdr, void *data)
 {
-	struct llog_changelog_user_rec *rec;
+	struct llog_changelog_user_rec2 *rec;
 	struct seq_file *m = data;
+	char user_name[CHANGELOG_USER_NAMELEN_FULL];
 
 	LASSERT(llh->lgh_hdr->llh_flags & LLOG_F_IS_PLAIN);
 
-	rec = (struct llog_changelog_user_rec *)hdr;
+	rec = container_of(hdr, typeof(*rec), cur_hdr);
 
-	seq_printf(m, CHANGELOG_USER_PREFIX"%-3d %llu (%u)\n",
-		   rec->cur_id, rec->cur_endrec, (__u32)get_seconds() -
-						 rec->cur_time);
+	seq_printf(m, "%-24s %10llu (%u)",
+		   mdd_chlg_username(rec, user_name, sizeof(user_name)),
+		   rec->cur_endrec,
+		   (__u32)ktime_get_real_seconds() - rec->cur_time);
+	if (mdd_chlg_usermask(rec)) {
+		char *sep = "";
+		int i;
+
+		seq_puts(m, " mask=");
+		for (i = 0; i < CL_LAST; i++) {
+			if (!(mdd_chlg_usermask(rec) & BIT(i)))
+				continue;
+			if (*sep)
+				seq_puts(m, sep);
+			seq_puts(m, changelog_type2str(i));
+			sep = ",";
+		}
+	}
+	seq_puts(m, "\n");
+
 	return 0;
 }
 
@@ -167,8 +241,8 @@ static int mdd_changelog_users_seq_show(struct seq_file *m, void *data)
 	cur = mdd->mdd_cl.mc_index;
 	spin_unlock(&mdd->mdd_cl.mc_lock);
 
-	seq_printf(m, "current index: %llu\n", cur);
-	seq_printf(m, "%-5s %s %s\n", "ID", "index", "(idle seconds)");
+	seq_printf(m, "current_index: %llu\n", cur);
+	seq_printf(m, "%-24s %10s %s %s\n", "ID", "index", "(idle)", "mask");
 
 	llog_cat_process(&env, ctxt->loc_handle, lprocfs_changelog_users_cb,
 			 m, 0, 0);
@@ -646,6 +720,8 @@ LUSTRE_RW_ATTR(append_pool);
 static struct ldebugfs_vars ldebugfs_mdd_obd_vars[] = {
 	{ .name =	"changelog_mask",
 	  .fops =	&mdd_changelog_mask_fops	},
+	{ .name =	"changelog_current_mask",
+	  .fops =	&mdd_changelog_current_mask_fops },
 	{ .name =	"changelog_users",
 	  .fops =	&mdd_changelog_users_fops	},
 	{ .name =	"lfsck_namespace",

lustre/mdd/mdd_object.c
@@ -1088,7 +1088,7 @@ static int mdd_attr_set_changelog(const struct lu_env *env,
 	bits |= (valid & LA_MTIME) ? BIT(CL_MTIME) : 0;
 	bits |= (valid & LA_CTIME) ? BIT(CL_CTIME) : 0;
 	bits |= (valid & LA_ATIME) ? BIT(CL_ATIME) : 0;
-	bits = bits & mdd->mdd_cl.mc_mask;
+	bits = bits & mdd->mdd_cl.mc_current_mask;
 	/* This is an implementation limit rather than a protocol limit */
 	BUILD_BUG_ON(CL_LAST > sizeof(int) * 8);
 	if (bits == 0)
@@ -3260,7 +3260,7 @@ static int mdd_open(const struct lu_env *env, struct md_object *obj,
 
 	rc = mdd_open_sanity_check(env, mdd_obj, attr, open_flags,
 				   spec->no_create);
-	if ((rc == -EACCES) && (mdd->mdd_cl.mc_mask & BIT(CL_DN_OPEN)))
+	if ((rc == -EACCES) && (mdd->mdd_cl.mc_current_mask & BIT(CL_DN_OPEN)))
 		type = CL_DN_OPEN;
 	else if (rc != 0)
 		GOTO(out, rc);
@@ -3498,10 +3498,11 @@ out:
 	 * this is not a big deal if we have a CL_CLOSE entry with no matching
 	 * CL_OPEN. Plus Changelogs mask may not change often.
 	 */
-	if (((!(mdd->mdd_cl.mc_mask & BIT(CL_OPEN)) &&
+	if (((!(mdd->mdd_cl.mc_current_mask & BIT(CL_OPEN)) &&
 	      (open_flags & (MDS_FMODE_WRITE | MDS_OPEN_APPEND |
 			     MDS_OPEN_TRUNC))) ||
-	     ((mdd->mdd_cl.mc_mask & BIT(CL_OPEN)) && last_close_by_uid)) &&
+	     ((mdd->mdd_cl.mc_current_mask & BIT(CL_OPEN)) &&
+	      last_close_by_uid)) &&
 	    !(ma->ma_valid & MA_FLAGS && ma->ma_attr_flags & MDS_RECOV_OPEN)) {
 		if (handle == NULL) {
 			handle = mdd_trans_create(env, mdo2mdd(obj));

lustre/mdd/mdd_trans.c
@@ -77,7 +77,7 @@ struct mdd_changelog_gc {
 	__u32 mcgc_id;
 	__u32 mcgc_maxtime;
 	__u64 mcgc_maxindexes;
-	bool mcgc_found;
+	char mcgc_name[CHANGELOG_USER_NAMELEN_FULL];
 };
 
 /* return first registered ChangeLog user idle since too long
@@ -86,16 +86,16 @@ static int mdd_changelog_gc_cb(const struct lu_env *env,
 			       struct llog_handle *llh,
 			       struct llog_rec_hdr *hdr, void *data)
 {
-	struct llog_changelog_user_rec *rec;
-	struct mdd_changelog_gc *mcgc = (struct mdd_changelog_gc *)data;
+	struct llog_changelog_user_rec2 *rec;
+	struct mdd_changelog_gc *mcgc = data;
 	struct mdd_device *mdd = mcgc->mcgc_mdd;
+
 	ENTRY;
 
 	if ((llh->lgh_hdr->llh_flags & LLOG_F_IS_PLAIN) == 0)
 		RETURN(-ENXIO);
 
-	rec = container_of(hdr, struct llog_changelog_user_rec,
-			   cur_hdr);
+	rec = container_of(hdr, typeof(*rec), cur_hdr);
 
 	/* find oldest idle user, based on last record update/cancel time (new
 	 * behavior), or for old user records, last record index vs current
@@ -103,9 +103,6 @@ static int mdd_changelog_gc_cb(const struct lu_env *env,
 	 * first as we assume they could be idle since longer
 	 */
 	if (rec->cur_time != 0) {
-		/* FIXME !!!! cur_time is a timestamp but only 32 bit in
-		 * in size. This is not 2038 safe !!!!
-		 */
 		u32 time_now = (u32)ktime_get_real_seconds();
 		timeout_t time_out = rec->cur_time +
 				     mdd->mdd_changelog_max_idle_time;
@@ -119,7 +116,8 @@ static int mdd_changelog_gc_cb(const struct lu_env *env,
 		    mcgc->mcgc_maxindexes == 0) {
 			mcgc->mcgc_maxtime = idle_time;
 			mcgc->mcgc_id = rec->cur_id;
-			mcgc->mcgc_found = true;
+			mdd_chlg_username(rec, mcgc->mcgc_name,
+					  sizeof(mcgc->mcgc_name));
 		}
 	} else {
 		/* old user record with no idle time stamp, so use empirical
@@ -134,7 +132,8 @@ static int mdd_changelog_gc_cb(const struct lu_env *env,
 		    idle_indexes > mcgc->mcgc_maxindexes) {
 			mcgc->mcgc_maxindexes = idle_indexes;
 			mcgc->mcgc_id = rec->cur_id;
-			mcgc->mcgc_found = true;
+			mdd_chlg_username(rec, mcgc->mcgc_name,
+					  sizeof(mcgc->mcgc_name));
 		}
 
 	}
@@ -144,16 +143,11 @@ static int mdd_changelog_gc_cb(const struct lu_env *env,
 /* recover space from long-term inactive ChangeLog users */
 static int mdd_chlg_garbage_collect(void *data)
 {
-	struct mdd_device *mdd = (struct mdd_device *)data;
-	struct lu_env		  *env = NULL;
-	int			   rc;
+	struct mdd_device *mdd = data;
+	struct lu_env *env = NULL;
+	int rc;
 	struct llog_ctxt *ctxt;
-	struct mdd_changelog_gc mcgc = {
-		.mcgc_mdd = mdd,
-		.mcgc_found = false,
-		.mcgc_maxtime = 0,
-		.mcgc_maxindexes = 0,
-	};
+
 	ENTRY;
 
 	mdd->mdd_cl.mc_gc_task = current;
@@ -170,6 +164,12 @@ static int mdd_chlg_garbage_collect(void *data)
 		GOTO(out, rc);
 
 	for (;;) {
+		struct mdd_changelog_gc mcgc = {
+			.mcgc_mdd = mdd,
+			.mcgc_maxtime = 0,
+			.mcgc_maxindexes = 0,
+		};
+
 		ctxt = llog_get_context(mdd2obd_dev(mdd),
 					LLOG_CHANGELOG_USER_ORIG_CTXT);
 		if (ctxt == NULL ||
@@ -178,30 +178,23 @@ static int mdd_chlg_garbage_collect(void *data)
 
 		rc = llog_cat_process(env, ctxt->loc_handle,
 				      mdd_changelog_gc_cb, &mcgc, 0, 0);
-		if (rc != 0 || mcgc.mcgc_found == false)
+		if (rc != 0 || !mcgc.mcgc_name[0])
 			break;
 		llog_ctxt_put(ctxt);
 
 		if (mcgc.mcgc_maxindexes != 0)
-			CWARN("%s: Force deregister of ChangeLog user cl%d "
-			      "idle with more than %llu unprocessed records\n",
-			      mdd2obd_dev(mdd)->obd_name, mcgc.mcgc_id,
+			CWARN("%s: Force deregister of ChangeLog user %s idle with more than %llu unprocessed records\n",
+			      mdd2obd_dev(mdd)->obd_name, mcgc.mcgc_name,
 			      mcgc.mcgc_maxindexes);
 		else
-			CWARN("%s: Force deregister of ChangeLog user cl%d "
-			      "idle since more than %us\n",
-			      mdd2obd_dev(mdd)->obd_name, mcgc.mcgc_id,
+			CWARN("%s: Force deregister of ChangeLog user %s idle since more than %us\n",
+			      mdd2obd_dev(mdd)->obd_name, mcgc.mcgc_name,
 			      mcgc.mcgc_maxtime);
 
 		mdd_changelog_user_purge(env, mdd, mcgc.mcgc_id);
 
 		if (kthread_should_stop())
 			GOTO(out_env, rc = 0);
-
-		/* try again to search for another candidate */
-		mcgc.mcgc_found = false;
-		mcgc.mcgc_maxtime = 0;
-		mcgc.mcgc_maxindexes = 0;
 	}
 
 out_ctxt:

commit 0b1e4c79711423441635ae832b302baf1006a4f3

LU-19704 mdd: process all changelog users during init
changelog_user_init_cb(), called during server initialization, is
responsible for processing all previously registered changelog users
and computing the union of their masks so that the correct changelog
records are kept.

However, it was returning LLOG_PROC_BREAK immediately and so would
only process a single changelog user. This meant that after
remounting an MDT, changelog records could be missed.

It looks like changelog_user_init_cb() has been returning
LLOG_PROC_BREAK since it was first implemented in 2c74bfcb7a0
("multiple changelog consumer registration"). At that time, this was
likely correct behavior.

However, a15eb4f13224 ("per-user changelog names and mask") introduced
a per-user mask and updated changelog_user_init_cb() to process each
user's mask during server init. That commit probably should have
updated the function to return 0 so that all users would be processed.

Fixes: a15eb4f13224 ("LU-13055 mdd: per-user changelog names and mask")
Signed-off-by: Thomas Bertschinger <bertschinger@lanl.gov>
Change-Id: Ic498be0a43d6d22c7b9d2ba9de2aa8560f1c5b5d

lustre/mdd/mdd_device.c
@@ -277,7 +277,7 @@ static int changelog_user_init_cb(const struct lu_env *env,
 		mdd->mdd_cl.mc_index = rec->cur_endrec;
 	spin_unlock(&mdd->mdd_cl.mc_lock);
 
-	return LLOG_PROC_BREAK;
+	return 0;
 }
 
 struct changelog_orphan_data {

lustre/tests/sanity.sh
@@ -21103,6 +21103,43 @@ test_160u() { # LU-17400
 }
 run_test 160u "changelog rename record type name and sname strings are correct"
 
+test_160v() {
+	[ $PARALLEL == "yes" ] && skip "skip parallel run"
+	remote_mds_nodsh && skip "remote MDS with nodsh"
+
+	changelog_register --user test_160v_mkdir -m mkdir ||
+		error "mkdir changelog_register failed"
+
+	changelog_register --user test_160v_creat -m creat ||
+		error "creat changelog_register failed"
+
+	stop mds1
+	start mds1 $(mdsdevname 1) $MDS_MOUNT_OPTS || error "cannot start mds1"
+
+	local mask=$(do_facet mds1 lctl get_param mdd.$FSNAME-MDT0000.changelog_current_mask)
+
+	[[ "$mask" =~ "MKDIR" ]] ||
+		error "changelog mask should contain MKDIR"
+
+	[[ "$mask" =~ "CREAT" ]] ||
+		error "changelog mask should contain CREAT"
+
+	local dir="$DIR/$tdir"
+	$LFS mkdir -i0 $dir || error "lfs mkdir failed"
+
+	changelog_clear 0 || error "changelog_clear failed"
+
+	touch $dir/testfile || error "touch failed"
+	mkdir $dir/testdir || error "mkdir failed"
+
+	local records=$(changelog_dump)
+	echo $records | grep "MKDIR" ||
+		error "changelog should include MKDIR record"
+	echo $records | grep "CREAT" ||
+		error "changelog should include CREAT record"
+}
+run_test 160v "load multiple changelog users after remount"
+
 test_161a() {
 	[ $PARALLEL == "yes" ] && skip "skip parallel run"
 